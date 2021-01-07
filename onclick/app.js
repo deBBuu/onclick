b9e944bbbe0996b7aa4620d98954ffae4681c2a9
deBBuu <62139065+deBBuu@users.noreply.github.com>
2021-01-07 11:58:15 +0100

    Add files via upload

---
+++ onclick/app.js
@@ -0,0 +1,31 @@
+function changeSizeOne(){
+    document.getElementById('circle1').style.width="20vw"
+    document.getElementById('circle1').style.height="20vw"
+
+    document.getElementById('circle3').style.width="15vw"
+    document.getElementById('circle3').style.height="15vw"
+    document.getElementById('circle2').style.width="15vw"
+    document.getElementById('circle2').style.height="15vw"
+}
+
+function changeSizeTwo(){
+    document.getElementById('circle2').style.width="20vw"
+    document.getElementById('circle2').style.height="20vw"
+
+    document.getElementById('circle1').style.width="15vw"
+    document.getElementById('circle1').style.height="15vw"
+    document.getElementById('circle3').style.width="15vw"
+    document.getElementById('circle3').style.height="15vw"
+
+
+}
+
+function changeSizeThree(){
+    document.getElementById('circle3').style.width="20vw"
+    document.getElementById('circle3').style.height="20vw"
+
+    document.getElementById('circle2').style.width="15vw"
+    document.getElementById('circle2').style.height="15vw"
+    document.getElementById('circle1').style.width="15vw"
+    document.getElementById('circle1').style.height="15vw"
+}
